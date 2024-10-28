d0c4ef0dc356adbd806f844de7e47917920006e1
erick show de bola <164902783+eerick66@users.noreply.github.com>
2024-10-28 12:40:14 -0300

    Add files via upload

---
+++ Erick/perguntas.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Quem sou eu?',
+    '',
+    'Sou o Erick, tenho 16 anos e luto muay thai'
+)
+
+criaCartao(
+    'Minha familia é?',
+    '',
+    'muito legal'
+)
+
+criaCartao(
+    'Onde moro?',
+    '',
+    'Moro em Pinhãp-PR no bairro Mazurechem'
+)
+
+criaCartao(
+    'Onde estudo?',
+    '',
+    'Estudo no Colegio Estadual Prof. Mario Evaldo Morski '
+)
+
+
+criaCartao(
+    'Minha turma?',
+    '',
+    'Loucos e divertidos'
+)
+
+
+criaCartao(
+    'Faculdade?',
+    '',
+    'quero seguir carreira militar '
+)
+
+
+criaCartao(
+    'Minha cidade?',
+    '',
+    'Uma cidade pouco desenvolvida mas tranquila'
+)
+
+
+criaCartao(
+    'Meu sonho?',
+    '',
+    'Construir uma familia e subir ao palco do mr olympia '
+)
